075abe8a369d53bdba7405abcc3c5f06029cbacc
Adam Crownoble <adam.crownoble@biola.edu>
2014-03-20 10:54:50 -0700

    Rake JSON formatted 404 errors

--- lib/trogdir/helpers/response_helpers.rb
+++ lib/trogdir/helpers/response_helpers.rb
@@ -1,11 +1,11 @@
 module Trogdir
   module ResponseHelpers
-    def raise_404(klass, conditions)
-      raise Mongoid::Errors::DocumentNotFound.new klass, conditions
+    def raise_404
+      error!('404 Not Found', 404)
     end
 
     def elem_match_or_404(klass, conditions)
-      klass.elem_match(conditions).first or raise_404(klass, conditions)
+      klass.elem_match(conditions).first or raise_404
     end
   end
 end
