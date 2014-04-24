5910e765f31137c35052608c9fc2e2bc2d466d30
Adam Crownoble <adam.crownoble@biola.edu>
2014-04-24 15:12:22 -0700

    Fix typo in config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 require 'bundler'
-Bundler.setup :default, ENV['RACK_ENV'] || ENV['RAILS_ENV'] || :developement
+Bundler.setup :default, ENV['RACK_ENV'] || ENV['RAILS_ENV'] || :development
 
 require_relative '../lib/trogdir_api'
 TrogdirAPI.initialize!
