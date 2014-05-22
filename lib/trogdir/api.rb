daff8fc685ec349103c4adb13eaec3bc9f3fe3b3
Adam Crownoble <adam.crownoble@biola.edu>
2014-05-22 15:39:44 -0700

    More detailed NewRelic stats

--- lib/trogdir/api.rb
+++ lib/trogdir/api.rb
@@ -1,5 +1,9 @@
+require 'new_relic/agent/instrumentation/rack'
+
 module Trogdir
   class API < Grape::API
     mount Trogdir::V1::API
+
+    include ::NewRelic::Agent::Instrumentation::Rack
   end
 end
